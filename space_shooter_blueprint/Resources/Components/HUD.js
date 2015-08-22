804cdb2232e1ff53d9f356fa9f462c7ed895b162
Shaddock Heath <shaddockh@gmail.com>
2015-08-22 08:51:02 -0500

    fix issues with ui

--- space_shooter_blueprint/Resources/Components/HUD.js
+++ space_shooter_blueprint/Resources/Components/HUD.js
@@ -4,7 +4,7 @@ function buildFontDescription(font, size) {
     var fd;
     fd = new Atomic.UIFontDescription();
     fd.id = font;
-    fd.size = size; 
+    fd.size = size;
     return fd;
 }
 module.exports.component = function (self) {
@@ -13,37 +13,57 @@ module.exports.component = function (self) {
 
     var layout = new Atomic.UILayout();
     layout.rect = view.rect;
-    //layout.axis = Atomic.UI_AXIS_Y;
-    layout.layoutPosition = Atomic.UI_LAYOUT_POSITION_GRAVITY;
-
+    layout.axis = Atomic.UI_AXIS_Y;
+    layout.layoutDistribution = Atomic.UI_LAYOUT_DISTRIBUTION_AVAILABLE;
+    layout.layoutDistributionSize = Atomic.UI_LAYOUT_SIZE_AVAILABLE;
+    layout.layoutSpacing = 0;
     view.addChild(layout);
 
-    //var uiStyle = Atomic.cache.getResource("XMLFile", "UI/DefaultStyle.xml");
-    //view.defaultStyle = uiStyle;
+    var layout2 = new Atomic.UILayout();
+    layout.layoutDistribution = Atomic.UI_LAYOUT_DISTRIBUTION_GRAVITY;
+    layout.addChild(layout2);
+
+    var container = new Atomic.UIContainer();
+    container.gravity = Atomic.UI_GRAVITY_LEFT_RIGHT;
+    layout2.addChild(container);
+
+    var layout3 = new Atomic.UILayout();
+    layout.layoutDistribution = Atomic.UI_LAYOUT_DISTRIBUTION_AVAILABLE;
+    layout.layoutDistributionSize = Atomic.UI_LAYOUT_SIZE_AVAILABLE;
+    container.addChild(layout3);
 
     var scoreText = new Atomic.UITextField();
     scoreText.text = "Score: 0";
     scoreText.setFontDescription(buildFontDescription('Vera', 24));
     scoreText.color = [0, 1, 0, 1];
-    scoreText.gravity = Atomic.UI_GRAVITY_RIGHT | Atomic.UI_GRAVITY_TOP;
-    layout.addChild(scoreText);
-
-
+    scoreText.gravity = Atomic.UI_GRAVITY_LEFT;
+    scoreText.textAlign = Atomic.UI_TEXT_ALIGN_LEFT;
+    scoreText.setLayoutParams((function () {
+        var lp = new Atomic.UILayoutParams();
+        lp.setWidth(192);
+        return lp;
+    }()));
+    layout3.addChild(scoreText);
 
     var titleText = new Atomic.UITextField();
     titleText.text = "Atomic Space Game";
-    titleText.setFontDescription(buildFontDescription('Vera', 18));
+    titleText.setFontDescription(buildFontDescription('Vera', 24));
     titleText.color = [0, 1, 0, 1];
-    titleText.gravity = Atomic.UI_GRAVITY_LEFT | Atomic.UI_GRAVITY_TOP;
-    layout.addChild(titleText);
-
+    titleText.gravity = Atomic.UI_GRAVITY_ALL
+    layout3.addChild(titleText);
 
     var healthText = new Atomic.UITextField();
     healthText.text = "Health: 10";
-    titleText.setFontDescription(buildFontDescription('Vera', 18));
+    healthText.setFontDescription(buildFontDescription('Vera', 24));
     healthText.color = [0, 1, 0, 1];
-    healthText.gravity = Atomic.UI_GRAVITY_RIGHT | Atomic.UI_GRAVITY_BOTTOM;
-    layout.addChild(healthText);
+    healthText.gravity = Atomic.UI_GRAVITY_RIGHT;
+    healthText.textAlign = Atomic.UI_TEXT_ALIGN_RIGHT;
+    healthText.setLayoutParams((function () {
+        var lp = new Atomic.UILayoutParams();
+        lp.setWidth(128);
+        return lp;
+    }()));
+    layout3.addChild(healthText);
 
     var gameText = new Atomic.UITextField();
     titleText.setFontDescription(buildFontDescription('Vera', 40));
