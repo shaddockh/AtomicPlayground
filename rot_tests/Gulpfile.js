09561c253bd82a81b4052c9d415632a96084e3d2
Shaddock Heath <shaddockh@gmail.com>
2015-09-05 08:07:21 -0500

    adding source map generation and eslint to the gulp build

--- rot_tests/Gulpfile.js
+++ rot_tests/Gulpfile.js
@@ -3,22 +3,38 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var babel = require("gulp-babel");
+var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var del = require('del');
 var replace = require('gulp-replace');
+var eslint = require('gulp-eslint');
 
 var paths = {
     babel: 'Resources/**/*.es6',
-    atomify: './vendor.js'
+    atomify: './vendor.js',
+    lint: 'Resources/**/*.{es6,js}'
 };
 
 gulp.task('clean', function (cb) {
     del(['build'], cb);
 });
 
+gulp.task('lint', function () {
+    return gulp.src(paths.lint)
+        // eslint() attaches the lint output to the eslint property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failOnError last.
+        .pipe(eslint.failOnError());
+});
+
 // We want to run all the .es6 files through babel first, before we browserify them
-gulp.task('babel', ['copy-files'], doBabel);
+gulp.task('babel', ['lint','copy-files'], doBabel);
 gulp.task('babel-watch', doBabel);
 gulp.task('atomify', ['copy-files'], doAtomify);
 gulp.task('atomify-watch', doAtomify);
@@ -68,11 +84,12 @@ function doAtomify() {
 
 function doBabel() {
     return gulp.src(paths.babel)
+        .pipe(sourcemaps.init())
         .pipe(babel({
             blacklist: ['strict'], // atomic doesn't like use strict in all cases.  If you want it, add it to the es6 file and it will carry over.
-            "sourceMaps": true,
             "only": ["*.es6"],
-            "optional": ["es7.classProperties"],
+            "optional": ["es7.classProperties"]
         }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest("./build/Resources"));
 }
